4ee274b4170cd5bf32bcdf734d7a063b915fb15a
baanuridwan <baanuridwan@gmail.com>
2024-11-11 09:26:32 +0100

    first deploy SQI

---
+++ sample.js
@@ -0,0 +1,19 @@
+function externalFunction(){
+    alert('This is coming from an external JS file')
+}
+   // let const var 
+    // Console=> Ways of outputing data in JS
+    // Alert => Ways of outputing data in JS
+    // innerHTML=> Ways of outputing data in JS
+    // let const var 
+    let okay="My name is Shammah"
+    let lorem= "lorem ipsum delirium"
+    document.getElementById('show').innerHTML = okay
+    document.getElementById('lorem').innerHTML = lorem
+    console.log()
+    alert(okay)
+    
+        
+        // identifier and the value
+    
+        console.log()
